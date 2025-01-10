88bc6cf1108ba1010505156277eabaf901d50e3b
Acosta Matias <aaco.maty@gmail.com>
2025-01-10 16:13:54 -0300

    0.5

--- src/util/scripts/automap.py
+++ src/util/scripts/automap.py
@@ -31,7 +31,7 @@ def main(image_path, output_js_path):
     skeleton_ubyte = img_as_ubyte(skeleton)
 
     # Guardar la imagen esqueletizada
-    cv2.imwrite("skeleton.png", skeleton_ubyte)
+    cv2.imwrite("generated_ske.png", skeleton_ubyte)
 
     skeleton_graph = skan.Skeleton(skeleton)
     graph = skan.csr.skeleton_to_nx(skeleton_graph)
@@ -56,7 +56,72 @@ def main(image_path, output_js_path):
 
     nx.set_edge_attributes(graph, edge_weights, 'weight')
 
-    plot_graph(image, graph, skeleton_graph, "Grafo")
+    plot_graph(image, graph, skeleton_graph, "Grafo antes de eliminar nodos")
+
+    # Nodos a eliminar
+    nodes_to_remove = [
+{ "source": 3687, "target": 3687 },
+{ "source": 3807, "target": 3807 },
+{ "source": 55676, "target": 55676 },
+{ "source": 45360, "target": 45360 },
+{ "source": 4680, "target": 4680 },
+{ "source": 2348, "target": 2348 },
+{ "source": 659, "target": 659 },
+{ "source": 21566, "target": 21566 },
+{ "source": 160167, "target": 160167 },
+{ "source": 178819, "target": 178819 },
+{ "source": 151507, "target": 151507 },
+{ "source": 164898, "target": 164898 },
+{ "source": 118505, "target": 118505 },
+{ "source": 107345, "target": 107345 },
+{ "source": 46485, "target": 46485 },
+{ "source": 117220, "target": 117220 },
+{ "source": 135623, "target": 135623 },
+{ "source": 116929, "target": 116929 },
+{ "source": 32555, "target": 32555 },
+{ "source": 35199, "target": 35199 },
+{ "source": 44963, "target": 44963 },
+{ "source": 43021, "target": 43021 },
+{ "source": 100116, "target": 100116 },
+{ "source": 52191, "target": 52191 },
+{ "source": 39268, "target": 39268 },
+{ "source": 42971, "target": 42971 },
+{ "source": 81501, "target": 81501 },
+{ "source": 89929, "target": 89929 },
+{ "source": 92253, "target": 92253 },
+{ "source": 127466, "target": 127466 },
+{ "source": 130976, "target": 130976 },
+{ "source": 132281, "target": 132281 },
+{ "source": 128730, "target": 128730 },
+{ "source": 133248, "target": 133248 },
+{ "source": 146967, "target": 146967 },
+{ "source": 162924, "target": 162924 },
+{ "source": 90122, "target": 90122 },
+{ "source": 86566, "target": 86566 },
+{ "source": 85138, "target": 85138 },
+{ "source": 57468, "target": 57468 },
+{ "source": 135307, "target": 135307 },
+{ "source": 134487, "target": 134487 },
+{ "source": 155401, "target": 155401 },
+{ "source": 160173, "target": 160173 },
+{ "source": 165409, "target": 165409 },
+{ "source": 163228, "target": 163228 },
+{ "source": 174193, "target": 174193 },
+{ "source": 178432, "target": 178432 },
+{ "source": 174389, "target": 174389 },
+{ "source": 183522, "target": 183522 },
+{ "source": 183102, "target": 183102 },
+{ "source": 183907, "target": 183907 },
+{ "source": 26933, "target": 26933 }
+]
+
+    # Eliminar nodos del grafo
+    for node_data in nodes_to_remove:
+        node = node_data["source"]
+        if graph.has_node(node):  # Verificar si el nodo existe en el grafo
+            graph.remove_node(node)
+
+    plot_graph(image, graph, skeleton_graph, "Grafo después de eliminar nodos")
 
     # Convertir nodos y aristas a un formato adecuado para JavaScript
     nodes = []
@@ -94,7 +159,7 @@ export const graph_data = {{
 
 
 # Ruta de la imagen del mapa y salida JavaScript
-image_path = r"F:\VSCODE-Repos\gta-v-webmap\src\util\map_image.png"
+image_path = r"F:\VSCODE-Repos\gta-v-webmap\src\util\map_skeleton.png"
 output_js_path = r"F:\VSCODE-Repos\gta-v-webmap\src\util\graph_data.js"
 
 main(image_path, output_js_path)
